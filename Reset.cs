26ec69cdd8fc5efb220fd96a44f3de915bb66a51
UMA <55398160+umarchi@users.noreply.github.com>
2020-09-23 09:53:01 +0900

    Update Reset.cs

---
+++ Reset.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(Keycode.Space))
+        {
+            transform.position = new Vector3(0.0f, 3.0f, 0.0f);
+        }
+    }
+}
